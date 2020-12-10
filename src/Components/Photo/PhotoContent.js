f8723e0ef90429197d5082a480cdb2e57a35dd94
gabrielxpinheiro <gabrielpinheiro77@gmail.com>
2020-12-10 10:20:22 -0300

    feed modal styles

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
-import styles from './PhotoContent.module.css';
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
@@ -10,19 +10,20 @@ const PhotoContent = ({ data }) => {
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
-      <div className={styles.datails}></div>
-      <div>
-        <p>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-          <span className={styles.visualizacoes}>{photo.acessos}</span>
-        </p>
-        <h1 className="title">
-          <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-        </h1>
-        <ul className={styles.attributes}>
-          <li>{photo.peso} kg</li>
-          <li>{photo.idade} anos</li>
-        </ul>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
       </div>
       <PhotoComments id={photo.id} comments={comments} />
     </div>
